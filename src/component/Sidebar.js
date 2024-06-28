d2fa619f95e86770075e16757a558b5b55d079cd
Prashnt Sachan <swepra04@gmail.com>
2024-06-28 14:46:50 +0530

    spme  changes done

--- src/component/Sidebar.js
+++ src/component/Sidebar.js
@@ -2,50 +2,87 @@ import React, { useContext, useState } from "react";
 import { ColorContext } from "./ColorProvider";
 import ColorPicker from "../page/ColorPicker";
 import LogoChanger from "../page/LogoChanger";
-
+import { RxCross2 } from "react-icons/rx";
 const Sidebar = () => {
     const [isOpenColor, setIsOpenColor] = useState(false);
-    const [isOpenLogo, setIsOpenLogo] = useState(false)
-    const [showModal, setShowModal] = useState(false)
-    const { color } = useContext(ColorContext)
+    const [isOpenLogo, setIsOpenLogo] = useState(false);
+    const [showModal, setShowModal] = useState(false);
+    const { color } = useContext(ColorContext);
     return (
         <div>
             <div className="fixed flex gap-2 flex-col right-0 top-1/2 text-white z-10">
-                <button className=" px-4 py-2 z-10 " style={{ backgroundColor: color }} onClick={() => setIsOpenColor(!isOpenColor)}>
+                <button
+                    className=" px-4 py-2 z-10 "
+                    style={{ backgroundColor: color }}
+                    onClick={() => setIsOpenColor(!isOpenColor)}
+                >
                     Select Color
                 </button>
-                <button className=" px-4 py-2 z-10 " style={{ backgroundColor: color }} onClick={() => setIsOpenLogo(!isOpenLogo)}>
+                <button
+                    className=" px-4 py-2 z-10 "
+                    style={{ backgroundColor: color }}
+                    onClick={() => setIsOpenLogo(!isOpenLogo)}
+                >
                     Select Logo
                 </button>
-                <button className=" px-4 py-2 z-10 " style={{ backgroundColor: color }} onClick={() => setShowModal(!showModal)}>
+                <button
+                    className=" px-4 py-2 z-10 "
+                    style={{ backgroundColor: color }}
+                    onClick={() => setShowModal(!showModal)}
+                >
                     Submit
                 </button>
             </div>
 
-
-            {
-                isOpenColor &&
-                <div className="fixed border right-0 top-0 w-64 h-full bg-gray-800 text-white p-4">
-                    <button className="btn border px-2 ml-auto block ">*</button>
+            {isOpenColor && (
+                <div className="fixed border right-0 top-0 w-xl h-full bg-[#f2f2f2] text-black p-4">
+                    <button className="font-bold px-2 ml-auto block text-white p-2 rounded-lg " style={{ backgroundColor: color }} onClick={() => setIsOpenColor(!isOpenColor)}>
+                        <RxCross2 />
+                    </button>
                     <ColorPicker />
                 </div>
-            }
-            {isOpenLogo &&
-                <div className="fixed border right-0 top-0 w-64 h-full bg-gray-800 text-white p-4">
-                    <button className="btn border px-2 ml-auto block ">*</button>
+            )}
+            {isOpenLogo && (
+                <div className="fixed border right-0 top-0 w-1/3 h-full bg-gray-800 text-white p-4">
+                    <button className="font-bold px-2 ml-auto block text-white p-2 rounded-lg " style={{ backgroundColor: color }} onClick={() => setIsOpenLogo(!isOpenLogo)}>
+                        <RxCross2 />
+                    </button>
                     <LogoChanger />
                 </div>
-            }
-            {
-                showModal &&
-                < div className='border'>
-                    <p> When you submit this form our team will receive a notification with the color you selected and the logo you designed.</p>
-                    <input placeholder='Company' />
-                    <input placeholder='Name'></input>
-                </div>
+            )}
+            {showModal && (
+                <div className="border max-w-sm rounded-lg fixed transform -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 p-5 flex flex-col">
+                    <p className="font-bold">
+                        When you submit this form our team will receive a notification with
+                        the color you selected and the logo you designed.
+                    </p>
+                    <div className="flex flex-col gap-2">
+                        <input placeholder="Company" className="p-1 border rounded-lg" />
+                        <input placeholder="Name" className="p-1 border rounded-lg" />
+                        <input placeholder="Email" className="p-1 border rounded-lg" />
+                        <textarea
+                            placeholder="Comments"
+                            className="p-1 border rounded-lg h-20"
 
-            }
-        </div >
+                        />
+                    </div>
+                    <div className="flex justify-between mt-3">
+                        <button
+                            className="text-white px-4 py-2 rounded-md"
+                            style={{ backgroundColor: color }}
+                        >
+                            Submit
+                        </button>
+                        <button
+                            className="text-white px-4 py-2 rounded-md"
+                            style={{ backgroundColor: color }}
+                        >
+                            Cancel
+                        </button>
+                    </div>
+                </div>
+            )}
+        </div>
     );
 };
 
--- src/page/ColorPicker.js
+++ src/page/ColorPicker.js
@@ -2,15 +2,18 @@ import React, { useContext } from 'react'
 import { ColorContext } from '../component/ColorProvider';
 
 const ColorPicker = () => {
-    const colors = ['green', '#f44336', '#e91e63', '#9c27b0', '#673ab7'];
+    const colors = ['green', '#f44336', '#e91e63', '#9c27b0', '#673ab7', '#5083af', '#8fce00', '#ffe599', '#e06666'];
     const { color, setColor } = useContext(ColorContext)
     console.log(color)
     return (
         <div>
-            <h2>Pick a color</h2>
-            <div className='flex gap-4 flex-wrap' >
+            <h2 className='font-bold'>Filter</h2>
+            <input placeholder='Search...' className='p-1' />
+            <h1 className='font-bold text-md' >Select a color button below </h1>
+            <div className='border-black border-b-1' ></div>
+            <div className='flex gap-4 flex-wrap my-2' >
                 {colors.map((colorItem, index) => (
-                    <button key={index} className='btn border px-4 py-2' style={{ backgroundColor: colorItem }} onClick={() => setColor(colorItem)}>{colorItem}</button>
+                    <button key={index} className='btn border px-4 py-2 rounded-lg text-white' style={{ backgroundColor: colorItem }} onClick={() => setColor(colorItem)}>{colorItem}</button>
                 ))}</div>
         </div>
     )
